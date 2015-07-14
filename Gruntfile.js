b1a7c0fe896e90f329f18d1cb81f4aa5a54d6295
sixertoy <sixertoy.github@gmail.com>
2015-07-14 16:51:55 +0200

    add bower

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+/**
+ * <project_name>
+ * <project_repo>
+ *
+ * Copyright (c) <year> <username>
+ * Licensed under the MIT license.
+ *
+ */
+/*jslint indent: 4 */
+/*global module, require */
+module.exports = function (grunt) {
+    'use strict';
+    require('load-grunt-config')(grunt);
+};
---
+++ grunt/bump.js
@@ -0,0 +1,21 @@
+/*jslint indent: 4 */
+/*global module */
+module.exports = function (grunt, options) {
+    'use strict';
+    return {
+        options: {
+            files: ['package.json'],
+            updateConfigs: [],
+            commit: false,
+            commitMessage: '<%= bump.type %>(<%= bump.scope %>): <%= bump.subject %>',
+            commitFiles: ['-a'],
+            createTag: false,
+            tagName: 'v%VERSION%',
+            tagMessage: 'Version %VERSION%',
+            push: false,
+            pushTo: 'origin',
+            gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+            globalReplace: false
+        }
+    };
+};
